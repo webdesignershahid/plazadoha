339903b082bf243777f4780ffac262ecc17844e9
Shahidul Islam <webdesignershahid@gmail.com>
2024-01-12 02:02:21 +0600

    markup done

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,16 +64,16 @@
         /* ============================================================ */
         swiperCarousel: function () {
 
-            // Homepage Signup Side Slider
+            // Homepage Hero Slider
             let heroSlider = new Swiper ('#heroSlider', {
                 spaceBetween: 0,
                 slidesPerView: 1,
                 loop: true,
                 speed: 800,
                 effect: 'fade',
-                // autoplay: {
-                //     delay: 5000
-                // },
+                autoplay: {
+                    delay: 5000
+                },
                 // breakpoints: {
                 //     576: {
                 //         slidesPerView: 2,
@@ -92,6 +92,41 @@
                 //     nextEl: ".cabinet-carousel .swiper .swiper-button-next",
                 //     prevEl: ".cabinet-carousel .swiper .swiper-button-prev",
                 // },
+            });
+            // Homepage Dining Slider
+            let diningSlider = new Swiper ('#dining_slider', {
+                spaceBetween: 30,
+                slidesPerView: 1,
+                loop: true,
+                speed: 800,
+                autoplay: {
+                    delay: 5000
+                },
+                breakpoints: {
+                    576: {
+                        slidesPerView: 2,
+                    },
+                    992: {
+                        slidesPerView: 3,
+                    },
+                },
+            });
+            let amenitiesSlider = new Swiper ('#amenitiesSlider', {
+                spaceBetween: 30,
+                slidesPerView: 1,
+                loop: true,
+                speed: 800,
+                autoplay: {
+                    delay: 5000
+                },
+                breakpoints: {
+                    576: {
+                        slidesPerView: 2,
+                    },
+                    992: {
+                        slidesPerView: 3,
+                    },
+                },
             });
             
             // const lightbox = GLightbox({
@@ -128,7 +163,13 @@
         /* Scroll Top
         /* ============================================================ */
         scroll_to_top: function() {
-            $('body').append("<a href='javascript:void(0)' title='Scroll Top' id='scroll-top' class='topbutton btn-hide'></a>");
+            $('body').append(
+                `<a href='javascript:void(0)' title='Scroll Top' id='scroll-top' class='topbutton btn-hide'>
+                    <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.8955 9.89901C19.8645 9.93089 19.8271 9.9563 19.7856 9.97367C19.7441 9.99104 19.6993 10 19.6541 10C19.6088 10 19.5641 9.99104 19.5226 9.97367C19.481 9.9563 19.4436 9.93089 19.4127 9.89901L9.99774 0.807515L0.582812 9.89901C0.551109 9.92964 0.513473 9.95395 0.472052 9.97053C0.430632 9.98711 0.386237 9.99564 0.341403 9.99564C0.296569 9.99564 0.252175 9.98711 0.210754 9.97053C0.169333 9.95395 0.131697 9.92964 0.0999946 9.89901C0.0682924 9.86837 0.0431448 9.832 0.0259877 9.79198C0.00883061 9.75195 0 9.70905 0 9.66572C0 9.6224 0.00883061 9.5795 0.0259877 9.53947C0.0431448 9.49944 0.0682924 9.46307 0.0999946 9.43244L9.75633 0.100991C9.7873 0.0691107 9.82469 0.0437013 9.86621 0.0263313C9.90773 0.00896132 9.9525 -4.72692e-08 9.99774 -4.72692e-08C10.043 -4.72692e-08 10.0878 0.00896132 10.1293 0.0263313C10.1708 0.0437013 10.2082 0.0691107 10.2392 0.100991L19.8955 9.43244C19.9285 9.46236 19.9548 9.49849 19.9728 9.53862C19.9907 9.57874 20 9.622 20 9.66572C20 9.70944 19.9907 9.7527 19.9728 9.79283C19.9548 9.83295 19.9285 9.86909 19.8955 9.89901Z" fill="black"/>
+                    </svg>
+                </a>`
+            );
             var $scrolltop = $('#scroll-top');
             $(window).on('scroll', function () {
                 if ($(this).scrollTop() > $(this).height()) {
